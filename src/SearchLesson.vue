aead0a2226fcc07eedb56d2982671a4aac1dc024
danportchen <danport.chen@gmail.com>
2018-09-08 19:30:30 +0800

    Search.vue

---
+++ src/SearchLesson.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <input type="text" v-model="text"/>
+    <div class="lesson-list">
+      <div class="lesson" v-for="lesson in lessons" :key="lesson">
+        <img :src="lesson.cover"/>
+        <span>{{ lesson.title }}</span>
+        <label>{{ lesson.owner.username}}</label>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import debounce from 'lodash/debounce';
+  const SEARCH_API = 'https://hiskio.com/api/v1/courses/search?word=';
+  export default{
+    data(){
+      return {
+        text: '',
+        lesson: [],
+      };
+    },
+    methods:{
+      search(val){
+        fetch(`${SEARCH_API}${val}`)
+        .then(rs => rs.json())
+        .then(data => {
+          this.lessons = data.courses;
+        });
+      },
+    },
+    watch:{
+      text(val){
+        this.searchDebounced(val);
+      },
+    },
+    created(){
+      this.searchDebounced = debounce(this.search, 50);
+    },
+  }
+</script>
+<style>
+.lesson img {
+  width: 100px;
+}
+.lesson span, .lesson label {
+  display: block;
+}
+</style>
